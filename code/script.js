506fc4e6df0a3ed773d735dc9377e49e620bd0a0
KaikyBruno <kaikybruno069@gmail.com>
2023-07-25 19:06:55 -0300

    create

--- code/script.js
+++ code/script.js
@@ -1,5 +1,22 @@
-const cepinput = document.querySelectorAll ('#cep')
+const cepinput = document.getElementById ('cep')
+const encontrarinput = document.getElementById ('encontrar')
+const voltar = document.getElementById ('voltar')
 
 cepinput.addEventListener ("keypress", (e) => {
+    const numbers = /[0-9]/
+    const key = String.fromCharCode (e.keyCode)
+
+    if (!numbers.test(key)) {
+        e.preventDefault ()
+        return
+    }
+})
+
+
+encontrarinput.addEventListener ("click", async(cep) => {
+    
+    const cepinputValue = cepinput.value
+
+    console.log (cepinputValue)
 
 })

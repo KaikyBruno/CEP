556fcda3ae5b87d69c46510bb79ca79e77522c40
KaikyBruno <kaikybruno069@gmail.com>
2023-07-28 21:15:15 -0300

    create

--- code/script.js
+++ code/script.js
@@ -1,6 +1,13 @@
 const cepinput = document.getElementById ('cep')
 const encontrarinput = document.getElementById ('encontrar')
-const voltarmensagem = document.getElementById ('voltar1')
+const buttonvoltar = document.getElementById ('voltar1')
+const buttonvoltarResp = document.getElementById ('resp')
+const respcep = document.getElementById ('respcep')
+const resplougradouro = document.getElementById ('logradouro')
+const respcomplemento = document.getElementById ('complemento')
+const respbairro = document.getElementById ('bairro')
+const resplocalidade = document.getElementById ('localidade')
+const respuf = document.getElementById ('uf')
 
 
 cepinput.addEventListener ("keypress", (e) => {
@@ -13,29 +20,43 @@ cepinput.addEventListener ("keypress", (e) => {
     }
 })
 
-async function clicar (cep) {
-    
+async function clicar () {
+
     event.preventDefault()
     const texterrado = document.getElementById ('errado')
 
     if (cepinput.value.length == '8') {
-        toggle ()
+
+        toggle()
+
         const apiUrl = `https://viacep.com.br/ws/${cepinput.value}/json/`;
         
         const response = await fetch(apiUrl)
         
         const data = await response.json()
-        
         console.log (data)
-        if (data.erro === 'true') {
+
+        toggle()        
+
+        if (data.erro === true) {
+           
+            mensagem()
+        
+        } else {
             
-            const mensagem = document.getElementById ('mensagem')
+            resposta(
 
-            mensagem.classList.toggle ('none')    
+                respcep.innerHTML = ` <strong>CEP:</strong> ${data.cep}`,
+                resplougradouro.innerHTML = ` <strong>Logradouro:</strong> ${data.logradouro}`,
+                respcomplemento.innerHTML = ` <strong>Complemento:</strong> ${data.complemento}`,
+                respbairro.innerHTML = ` <strong>Bairro:</strong> ${data.bairro}`,
+                resplocalidade.innerHTML = ` <strong>Localidade:</strong> ${data.localidade}`,
+                respuf.innerHTML = ` <strong>UF:</strong> ${data.uf}`
 
-            toggle()
-    }
+                
+            )
 
+        }
     } else {
         cepinput.style.outline = '1px solid red'
         cepinput.style.border = '1px solid red'
@@ -53,4 +74,33 @@ const toggle = () => {
     fade.classList.toggle ('none')
     toggle.classList.toggle ('none')
 }
+
+const mensagem = () => {
+
+    const mensagem = document.getElementById ('mensagem')
+    mensagem.style.display = 'block'
+
+    
+}
+
+const resposta = () => {
+
+    const resp = document.getElementById ('resp')
+    resp.style.display = 'block'
+
+    
+}
+
+buttonvoltar.addEventListener ("click", () => {
+
+    const mensagem = document.getElementById ('mensagem')
+    mensagem.style.display = 'none'
+})
+
+buttonvoltarResp.addEventListener ("click", () => {
+
+    const resp = document.getElementById ('resp')
+    resp.style.display = 'none'
+})
+
 
